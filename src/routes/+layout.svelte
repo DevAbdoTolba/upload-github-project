80a8eb68aa8c777975ad4c7ccd9fdf03db992e3e
Abdo Tolba <DevAbdoTolba@gmail.com>
2024-09-09 03:40:17 +0300

    firebase init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,44 @@
+<script>
+  import plantsWP from "$lib/images/plantsWP.jpg";
+</script>
+
+<svelte:head></svelte:head>
+
+<body style="background-image: url({plantsWP});">
+  <main class="main">
+    <slot />
+  </main>
+</body>
+
+<style>
+  * {
+    font-family: "Courier New", Courier, monospace;
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+    height: 100svh;
+    width: 100vw;
+  }
+
+  .main {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: rgba(255, 255, 255, 0.8);
+    padding: 20px;
+    border-radius: 10px;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,40 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div>
+  <h1>Login with github</h1>
+  <button class="btn">Login</button>
+</div>
+
+<style>
+  .btn {
+    /* modern clean soft nemorpihc design */
+    background-color: #f0f0f0;
+    height: fit-content;
+    width: 100%;
+    border: 1px solid #0f0f0f;
+    border-radius: 5px;
+    color: #333;
+    cursor: pointer;
+    display: inline-block;
+    font-size: 1rem;
+    font-weight: 400;
+    line-height: 1.5;
+    padding: 0.375rem 0.75rem;
+    text-align: center;
+    text-decoration: none;
+    transition:
+      background-color 0.15s,
+      border-color 0.15s,
+      color 0.15s;
+  }
+
+  .btn:hover {
+    background-color: #e6e6e6;
+    border-color: #e6e6e6;
+    color: #333;
+  }
+
+  .btn:active {
+    background-color: #d9d9d9;
+    border-color: #d9d9d9;
+    color: #333;
+  }
+</style>
