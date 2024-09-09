ac6ba455f3f5dde2b68678257b602f445d07d8e6
Abdo Tolba <DevAbdoTolba@gmail.com>
2024-09-09 08:56:49 +0300

    Auth working very well for the session and the new sessions!

---
+++ src/routes/user/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { user } from "../../stores/userStore";
+
+  let currentUser = null;
+
+  $: currentUser = $user;
+</script>
+
+{#if currentUser}
+  <h1>Welcome {currentUser.displayName}!</h1>
+{:else}
+  <h1>wot</h1>
+{/if}
